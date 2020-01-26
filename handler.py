131e5c1e4feb48742bbc3d2ba0bb7ad3bb1c45f7
paraz <mail2paras.s@gmail.com>
2020-01-26 21:34:54 +0530

    [Update] function, config

--- handler.py
+++ handler.py
@@ -1,8 +1,18 @@
-import json
+import re
+import urllib.request
+
+
+headers = {}
+headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    message = event['Records'][0]['Sns']['Message']
+    message = message.replace("\\r", " ").replace("\\n", " ")
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+    print("All urls:", urls)
+    if len(urls):
+        req = urllib.request.Request(urls[0], headers = headers)
+        resp = urllib.request.urlopen(req)
+        print("Request Status:", resp.status)
+    else:
+        print("No Url")
